9c699ae92c4fb7ed1f57b11d07a0d00fde127fd7
Stefan Sauer <ensonic@users.sf.net>
2014-08-11 23:44:37 +0200

    tests: fix binary location

--- tests/testmachine.sh
+++ tests/testmachine.sh
@@ -69,7 +69,7 @@ for machine in $machine_glob ; do
   #env >testmachine.log 2>&1 LD_LIBRARY_PATH="../src/" ../src/bmltest_info "$machine"
   #res=$?
   # this suppresses the output of e.g. "Sementation fault"
-  res=`env >bmltest_info.log 2>&1 BML_DEBUG=255 LD_LIBRARY_PATH="../src/:../src/BuzzMachineLoader/.libs:$LD_LIBRARY_PATH" ../src/bmltest_info "$machine"; echo $?`
+  res=`env >bmltest_info.log 2>&1 BML_DEBUG=255 LD_LIBRARY_PATH="../src/:../src/BuzzMachineLoader/.libs:$LD_LIBRARY_PATH" ../bmltest_info "$machine"; echo $?`
   cat bmltest_info.log | grep >"$log_name" -v "Warning: the specified"
   if [ $sig_int -eq "1" ] ; then res=1; fi
   cat bmltest_info.log | iconv >bmltest_info.tmp -fWINDOWS-1250 -tUTF-8 -c
@@ -93,7 +93,7 @@ for machine in $machine_glob ; do
     sig_segv=0
     sig_int=0
     # this suppresses the output of e.g. "Sementation fault"
-    res=`env >bmltest_process.log 2>&1 BML_DEBUG=255 LD_LIBRARY_PATH="../src/:../src/BuzzMachineLoader/.libs:$LD_LIBRARY_PATH" ../src/bmltest_process "$machine" input.raw output.raw; echo $?`
+    res=`env >bmltest_process.log 2>&1 BML_DEBUG=255 LD_LIBRARY_PATH="../src/:../src/BuzzMachineLoader/.libs:$LD_LIBRARY_PATH" ../bmltest_process "$machine" input.raw output.raw; echo $?`
     cat bmltest_process.log | grep >>"$log_name" -v "Warning: the specified"
     if [ $sig_int -eq "1" ] ; then res=1; fi
     if [ $res -eq "0" ] ; then
